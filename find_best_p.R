6f1bb69802beebe4d532bed867b926082d6a953a
Bangguo Wang <wbangguo8888@gmail.com>
2018-03-05 02:27:16 -0600

    Update find_best_p.R

--- find_best_p.R
+++ find_best_p.R
@@ -1,9 +1,9 @@
-find_best_p = function(p,y){
+find_best_p = function(p, y){
   # this function can help me to find the best threshold
   # p is the probability of positive class
   # y is the true label
   library(ROCR)
-  label = rep(0,length(y))
+  label = rep(0, length(y))
   label[y == '>50K'] = 1
   
   # apply the function from ROCR package
@@ -13,7 +13,7 @@ find_best_p = function(p,y){
   plot(f.perf)
   
   # find the best threshold that can maximize the f1-score
-  max_f1=max(f.perf@y.values[[1]][-1])
+  max_f1 = max(f.perf@y.values[[1]][-1])
   p_threshold = f.perf@x.values[[1]][f.perf@y.values[[1]][-1] == max_f1][[1]][1]
   
   return(p_threshold)
